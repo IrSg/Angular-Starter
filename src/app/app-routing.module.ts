e9769a1c7375fe48fa3635457df94b6d96cfc692
IrSg <winst1143@gmail.com>
2020-06-25 10:22:27 -0400

    create third component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { OurSecondComponentComponent } from './our-second-component/our-second-component.component';
-
+import { ThirdComponent } from './third/third.component';
 const routes: Routes = [
 { path: "", redirectTo: "home", pathMatch: 'full' }, //our home page will be the home route
 
-{path: "home", component: OurSecondComponentComponent } //this links the home route to the second component, so the second component becomes the home page
+{path: "home", component: OurSecondComponentComponent }, //this links the home route to the second component, so the second component becomes the home page
 
-
+{path: "third", component:ThirdComponent } 
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AmazingFirstComponentComponent } from './amazing-first-component/amazing-first-component.component';
 import { OurSecondComponentComponent } from './our-second-component/our-second-component.component';
+import { ThirdComponent } from './third/third.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AmazingFirstComponentComponent,
-    OurSecondComponentComponent
+    OurSecondComponentComponent,
+    ThirdComponent
   ],
   imports: [
     BrowserModule,
